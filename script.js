087b805b35fad7dfd6fda4ff0e640df20240a2ec
Vlad <107641467+Vla4is@users.noreply.github.com>
2024-11-11 22:03:35 +0200

    homework 8 addition

---
+++ script.js
@@ -0,0 +1,10 @@
+const make_button = (button_name) => {
+
+    document.getElementById("buttons").innerHTML += `<button onclick="window.location.href='${button_name}';">${button_name}</button>`;
+};
+
+let button_names = ['lw7', 'lw8'];
+button_names.forEach(button => {
+    
+    make_button(button);
+});
